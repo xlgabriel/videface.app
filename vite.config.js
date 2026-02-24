383ee669878f3a5d6255c0636261c59f5136d9ca
Johannes Herzog-Ahorn <48024528+Johannes910@users.noreply.github.com>
2026-02-24 15:09:00 -0500

    up
    UPDATE

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { visualizer } from 'rollup-plugin-visualizer'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({
+      open: true,       // abre el reporte automáticamente
+      gzipSize: true,   // muestra tamaño real comprimido
+      brotliSize: true, // muestra tamaño brotli
+    })
+  ],
 })
